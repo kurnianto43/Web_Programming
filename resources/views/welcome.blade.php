0e7fbff3ebc3ca4eef0c09d41cd9f3e7e53577d0
kurnianto43 <yanuarkurnianto1@gmail.com>
2017-11-01 01:54:16 +0800

    hapus file coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Beranda</a>
+                        <a href="{{ url('/home') }}">Berandai</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
