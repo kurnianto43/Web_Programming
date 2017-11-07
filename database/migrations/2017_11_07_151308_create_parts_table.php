4b1a0ae3a3a629574d1fdac73e3a111ba0586bb4
kurnianto43 <yanuarkurnianto1@gmail.com>
2017-11-08 03:16:39 +0800

    Membuat tabel parts dengan migration

--- database/migrations/2017_11_07_151308_create_parts_table.php
+++ database/migrations/2017_11_07_151308_create_parts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePartsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('parts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('no_spp', 10);
+            $table->string('nama_spp', 50);
+            $table->integer('jumlah');
+            $table->string('remarks', 100);
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('parts');
     }
 }
